93c9c498bc6d43ba6954c07605f60a35f7b4ccbd
AJ <jonnaajay999@gmail.com>
2022-10-28 13:25:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log(document);
+console.log(document.title);
+let title = document.getElementById("header-title");
+title.style.borderBottom = "solid 5px red";
+let item = document.querySelector(".title");
+item.style.color = "green";
+item.style.fontWeight = "bold";
