a42429ecbb87465d035d49c550f98887eb5dc1d2
ajayjonna9 <100123937+ajayjonna9@users.noreply.github.com>
2022-10-28 15:51:22 +0530

    secoend commit

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ title.style.borderBottom = "solid 5px red";
 let item = document.querySelector(".title");
 item.style.color = "green";
 item.style.fontWeight = "bold";
+let list = document.getElementsByClassName("list-group-item");
+console.log(list);
+list[2].style.backgroundColor = "green";
+for (let i = 0; i < list.length; i++) {
+  list[i].style.fontWeight = "bold";
+}
