1627bed726ed2bc3743ee44d323812d4e7816328
ajayjonna9 <100123937+ajayjonna9@users.noreply.github.com>
2022-10-28 16:21:23 +0530

    third commit

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ list[2].style.backgroundColor = "green";
 for (let i = 0; i < list.length; i++) {
   list[i].style.fontWeight = "bold";
 }
+let li = document.getElementsByTagName("li");
+console.log(li);
+for (let i = 0; i < li.length; i++) {
+  li[i].style.fontWeight = "bold";
+}
+let lielement = document.querySelector(".list-group-item");
+console.log(lielement);
+let a = document.querySelector(".list-group-item:nth-child(2)");
+a.style.color = "green";
+let lielements = document.querySelectorAll(".list-group-item");
+console.log(lielements);
+for (let i = 1; i < lielements.length; i = i + 2) {
+  lielements[i].style.backgroundColor = "red";
+}
+
